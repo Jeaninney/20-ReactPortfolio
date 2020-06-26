cdd66c4607061585e9c9444a4f8942693d840974
Jeaninne <jeaninnel@hotmail.com>
2020-06-25 21:48:29 -0400

    About.js works

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -47,3 +47,16 @@ function Footer() {
 }
 
 export default Footer;
+
+// import React from "react";
+// import "./style.css";
+
+// function Footer() {
+//   return (
+//     <footer className="footer">
+//       <span>George Washington Full Stack Developer Program -- Part Time -- January 2020</span>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
